9900bd8f64c240757e5d59a2a8581d8a9adea865
moamnhmdany.com <moamnhmdany2@gmail.com>
2022-04-03 23:01:39 +0300

    add new file

---
+++ src/screens/new.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const new = () => {
+  return (
+    <View>
+      <Text>new</Text>
+    </View>
+  )
+}
+
+export default new
