ee1f51bfc71ee79c0a001ec9a438dcc0bc3832f5
moamnhmdany.com <moamnhmdany2@gmail.com>
2022-04-04 21:46:19 +0300

    add collage names

--- src/components/card.js
+++ src/components/card.js
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text ,StyleSheet} from 'react-native'
 import React from 'react'
 
 const card = () => {
   return (
-    <View>
-      <Text>card</Text>
-    </View>
-  )
-}
+    <View style={{ ...styles.card, ...props.style }}>{props.children}</View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    width: "80%",
+    shadowColor: "black",
+    elevation: 5,
+    borderRadius: 10,
+    padding: 20,
+  },
+});
 
 export default card
